162bd57450406483165244e777707e3688d899e8
Calum Darroch <calum_darroch@hotmail.com>
2019-08-30 15:27:18 +0100

    Room size now stored as state when input, accessible via size function

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -10,7 +10,14 @@ describe('Feature test:', function(){
   it('rooms can be assigned X and Y coordinates sizes', function(){
     expect(room.inputSize(4, 5)).toEqual([4, 5]);
     expect(room.inputSize(7, 3)).toEqual([7, 3]);
-    expect(room.inputSize(19, 2)).toEqual([19, 2]);
+    expect(room.inputSize(19, 0)).toEqual([19, 0]);
     expect(room.inputSize(154, 896746)).toEqual([154, 896746]);
+  });
+
+  it('size of the room can be recorded after input', function(){
+    room.inputSize(12, 6);
+    expect(room.size()).toEqual([12, 6]);
+    room.inputSize(0, 2095234);
+    expect(room.size()).toEqual([0, 2095234]);
   });
 });
--- src/room.js
+++ src/room.js
@@ -1,7 +1,15 @@
 'use strict';
 
-function Room() {};
+function Room() {
+  this._size = [];
+};
 
-Room.prototype.inputSize = function(x, y){
+Room.prototype.inputSize = function(x, y) {
+  this._size[0] = x;
+  this._size[1] = y;
   return [x, y];
+};
+
+Room.prototype.size = function() {
+  return this._size;
 };
