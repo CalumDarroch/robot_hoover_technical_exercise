d6a1f4d09eacb480bd4769d1d73481ca0ecd73ce
Calum Darroch <calum_darroch@hotmail.com>
2019-09-01 21:50:13 +0100

    .txt file parsed into array of arrays

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello");
+'use strict';
+
+var fs = require('fs');
+var text = fs.readFileSync("./input.txt").toString("utf-8");
+var textByLine = text.split("\r\n");
+textByLine.pop();
+
+var hooverInput = [];
+var subArray
+
+var i;
+for(i = 0; i < textByLine.length; i++) {
+  if (i === textByLine.length - 1) {
+    subArray = textByLine[i].split("");
+    hooverInput.push(subArray);
+    } else {
+    subArray = textByLine[i].split(" ");
+    hooverInput.push(subArray);
+  };
+};
+
+console.log(hooverInput);
